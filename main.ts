5c1bfa2d43f882e68e89517b61f59298b1cba4ef
aryan251211 <81881363+aryan251211@users.noreply.github.com>
2021-06-11 19:18:18 -0400

    changed it to 2 sec to 5 sec

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ basic.forever(function () {
     for (let X = 0; X <= 5; X++) {
         for (let Y = 0; Y <= 5; Y++) {
             led.toggle(X, Y)
-            basic.pause(200)
+            basic.pause(500)
         }
     }
 })
